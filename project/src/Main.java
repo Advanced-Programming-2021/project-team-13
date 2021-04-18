9ab864f756e4549d5c2f14cdeb7994162a3f471e
MrNimaa <nima.na1991@gmail.com>
2021-04-19 00:09:45 +0430

    change view

--- project/src/Main.java
+++ project/src/Main.java
@@ -1,5 +1,8 @@
+import view.ViewMaster;
+
 public class Main {
     public static void main(String[] args) {
-
+        ViewMaster viewMaster = new ViewMaster();
+        viewMaster.run();
     }
 }
