0c342450d5fbe03c68390f65dfd54dafc1af7517
bigwhoman <hm.keshvari@gmail.com>
2021-04-26 06:39:07 +0430

    spell effect created , needs completion

---
+++ src/main/java/controll/SpellEffect.java
@@ -0,0 +1,95 @@
+package controll;
+
+public class SpellEffect {
+    public void monsterReborn() {
+
+    }
+
+    public void terraforming() {
+
+    }
+
+    public void potOfGreed() {
+
+    }
+
+    public void raigeki() {
+
+    }
+
+    public void changeOfHeart() {
+
+    }
+
+    public void harpie() {
+
+    }
+
+    public void swordsOfRevealing() {
+
+    }
+
+    public void darkHole() {
+
+    }
+
+    public void supplySquad() {
+
+    }
+
+    public void spellAbsorption() {
+
+    }
+
+    public void messengerOfPeace() {
+
+    }
+
+    public void twinTwisters() {
+
+    }
+
+    public void mysticalTyphoon() {
+
+    }
+
+    public void ringOfDefense() {
+
+    }
+
+    public void yami() {
+
+    }
+
+    public void forest() {
+
+    }
+
+    public void closedForest() {
+
+    }
+
+    public void umiiruka() {
+
+    }
+
+    public void swordOfDarkDestruction() {
+
+    }
+
+    public void blackPendant() {
+
+    }
+
+    public void unitedWeStand() {
+
+    }
+
+    public void magnumShield() {
+
+    }
+
+    public void advancedArt() {
+
+    }
+}
