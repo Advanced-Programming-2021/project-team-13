9b2a2b2bafa1fd4208a2e84701ad8aebf305437e
bigwhoman <hm.keshvari@gmail.com>
2021-07-03 12:09:30 +0430

    main

--- src/main/java/view/allmenu/MainView.java
+++ src/main/java/view/allmenu/MainView.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.effect.Bloom;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -45,15 +46,16 @@ public class MainView {
         MenuItem(String name, Runnable action) {
             LinearGradient gradient = new LinearGradient(
                     0, 0.5, 1, 0.5, true, CycleMethod.NO_CYCLE,
-                    new Stop(0.1, Color.web("black", 0.75)),
-                    new Stop(1.0, Color.web("black", 0.15)));
-            Rectangle bg = new Rectangle(250, 35, gradient);
+                    new Stop(0.1, Color.web("black", 1)),
+                    new Stop(1.0, Color.web("black", 0.5)));
+            Rectangle bg = new Rectangle(250, 38, gradient);
             Text text = new Text(name);
             text.setFont(Font.font("Verdana", FontWeight.BOLD, FontPosture.ITALIC, 22));
             setOnMouseClicked(e->{action.run(); });
             text.fillProperty().bind(
                     Bindings.when(hoverProperty()).then(Color.WHITE).otherwise(Color.GREY)
             );
+            text.setEffect(new Bloom(0.2));
             getChildren().addAll(bg, text);
         }
     }
@@ -71,9 +73,9 @@ public class MainView {
         );
         box.setTranslateX(1280 - 300);
         box.setTranslateY(200);
-        box.setBackground(new Background(new BackgroundFill(
-                Color.web("black", 0.6), null, null)
-        ));
+//        box.setBackground(new Background(new BackgroundFill(
+//                Color.web("black", 0.5), null, null)
+//        ));
         imageView=new ImageView();
         imageView.setImage(background1);
         pane.getChildren().addAll(
