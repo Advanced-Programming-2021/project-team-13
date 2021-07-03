dcf6a838a92e012f2707b432351c4f560cd49999
amrossein <amirbar2014@gmail.com>
2021-07-03 17:21:58 +0430

    adding deckMenu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,17 +7,20 @@ import view.SceneController;
 import view.ViewMaster;
 
 public class Main extends Application {
+
     public static void main(String[] args) {
         new UserJson().loadDataBase();
         new User("a","a","a");
         launch(args);
     }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 //        ViewMaster.getViewMaster().run();
         SceneController.startDeckMenu(primaryStage);
         primaryStage.show();
     }
+
     @Override
     public void stop(){
         new UserJson().update();
