c8610c080ca8678c8708cfd95c463af093c1fb16
MrNimaa <nima.na1991@gmail.com>
2021-07-04 01:11:36 +0430

    add ImageLoader

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 
+import controll.ImageLoader;
 import controll.json.UserJson;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -7,16 +8,19 @@ import view.ViewMaster;
 
 public class Main extends Application {
     public static void main(String[] args) {
+        new ImageLoader().load();
         new UserJson().loadDataBase();
-        new User("a","a","a");
+        new User("a", "a", "a");
         launch(args);
     }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         ViewMaster.getViewMaster().run();
     }
+
     @Override
-    public void stop(){
+    public void stop() {
         new UserJson().update();
     }
 }
