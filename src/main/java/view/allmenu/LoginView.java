d81ef1307c0cdc656ef509e39e43bc4eee32770d
bigwhoman <hm.keshvari@gmail.com>
2021-07-04 18:12:45 +0430

    login

--- src/main/java/view/allmenu/LoginView.java
+++ src/main/java/view/allmenu/LoginView.java
@@ -10,7 +10,10 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import model.menuItems.CustomButton;
 
 import java.awt.event.ActionEvent;
 import java.io.IOException;
@@ -19,11 +22,8 @@ import java.io.IOException;
 public class LoginView {
 
     private final LoginController loginController;
-    public Button loginButton;
     public TextField usernameField;
     public PasswordField passwordField;
-    public Button registerButton;
-    public Button exitButton;
     public Label notifLabel;
     public AnchorPane pane;
 
@@ -32,7 +32,35 @@ public class LoginView {
         loginController = new LoginController(this);
     }
 
-    public void initialize() throws IOException {
+    public void initialize(){
+        CustomButton exit = new CustomButton("Quit Game", this::exit);
+        CustomButton register= new CustomButton("Register", ()->{
+            try {
+                goToRegisterMenu();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        CustomButton login = new CustomButton("Login",()->{
+            try {
+                goToMainMenu();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        usernameField.setFont(Font.font("verdana", FontWeight.BOLD,22));
+        passwordField.setFont(Font.font("verdana", FontWeight.BOLD,22));
+        exit.setTranslateY(600);
+        exit.setTranslateX(100);
+        exit.setStyle("-fx-background-color: crimson");
+        login.setTranslateX(1280-350);
+        login.setTranslateY(200);
+        login.setStyle("-fx-background-color:#187c10");
+        register.setTranslateY(130);
+        register.setTranslateX(200);
+        register.setStyle("-fx-background-color:#1F51FF");
+        pane.getChildren().addAll(exit,login,register);
+
 
     }
 
@@ -50,16 +78,16 @@ public class LoginView {
         if (!login())
             return;
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainMenu.fxml"));
-        ((Stage) loginButton.getScene().getWindow()).setScene(new Scene(root));
+        ((Stage) pane.getScene().getWindow()).setScene(new Scene(root));
     }
 
     public void goToRegisterMenu() throws IOException {
         Parent parent = FXMLLoader.load(getClass().getResource("/fxml/RegisterMenu.fxml"));
-        ((Stage) registerButton.getScene().getWindow()).setScene(new Scene(parent));
+        ((Stage) pane.getScene().getWindow()).setScene(new Scene(parent));
     }
 
     public void exit() {
-        ((Stage)exitButton.getScene().getWindow()).close();
+        ((Stage)pane.getScene().getWindow()).close();
     }
 
 
