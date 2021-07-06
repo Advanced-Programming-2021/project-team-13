48ff0f99f27b2d78693a48fa014a2e0c17e4d6a5
bigwhoman <hm.keshvari@gmail.com>
2021-07-06 18:33:42 +0430

    frustrated af

--- src/main/java/view/allmenu/FookingTest.java
+++ src/main/java/view/allmenu/FookingTest.java
@@ -1,21 +1,33 @@
 package view.allmenu;
 
+import javafx.animation.FillTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.effect.Bloom;
+import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.security.Guard;
 
 public class FookingTest extends Application {
     public GridPane gridPane;
     public BorderPane motherPane;
-    public AnchorPane centerPane;
+    public StackPane centerPane;
     public StackPane stackPane;
+    public StackPane rivalGraveyard;
+    public StackPane ourField;
+    public StackPane rivalField;
+    public StackPane ourGraveyard;
 
     public static void main(String[] args) {
         launch(args);
@@ -29,32 +41,38 @@ public class FookingTest extends Application {
         primaryStage.show();
     }
     public void initialize() {
-        gridPane.getChildren().clear();
-//        for (int i = 0; i < ; i++) {
-//
-//        }
-        gridPane.setHgap(5);
-        gridPane.setVgap(5);
-        centerPane.getChildren().stream().filter(x -> x instanceof StackPane).forEach(x -> ((StackPane) x).setStyle("-fx-background-image: url('/shopImage/Monsters/AxeRaider.jpg') " +
-                ";-fx-background-repeat:no-repeat;-fx-background-size:cover,auto"));
-        centerPane.getChildren().stream()
-                .filter(x -> x instanceof GridPane).map(x -> (GridPane) x)
-                .forEach(x -> x.getChildren()
-                        .forEach(a -> a.setStyle("-fx-background-image: url('/shopImage/Monsters/AxeRaider.jpg') " +
-                                ";-fx-background-repeat:no-repeat no-repeat;-fx-background-size:cover,auto")));
-//        for (Node child : centerPane.getChildren()) {
-//            if(child instanceof StackPane) {
-//                ImageView bbb = new ImageView(background);
-//                ((StackPane) child).getChildren().add(bbb);
-//            }
-//            if(child instanceof GridPane){
-//                for (Node node : (((GridPane) child).getChildren())) {
-//                    if(node instanceof StackPane){
-//                        ImageView bbb = new ImageView(background);
-//                        ((StackPane) node).getChildren().add(bbb);
-//                    }
-//                }
-//            }
-//        }
+        centerPane.setAlignment(Pos.CENTER);
+        Image background = new Image("/shopImage/Monsters/AxeRaider.jpg");
+        for (int i = 0; i <5 ; i++) {
+            for (int j = 0; j < 4; j++) {
+                StackPane stackPane = new StackPane();
+                stackPane.setPrefWidth(140);
+                stackPane.setPrefHeight(190);
+                ImageView cardImages = new ImageView(background);
+                setEffectsCardImages(cardImages);
+                cardImages.setFitWidth(140);
+                cardImages.setFitHeight(190);
+                stackPane.getChildren().add(cardImages);
+                gridPane.add(stackPane,i,j);
+            }
+        }
+        gridPane.setTranslateX(20);
+        gridPane.setTranslateY(50);
+        gridPane.setHgap(10);
+        gridPane.setVgap(30);
+        centerPane.setStyle("-fx-background-image:url('/gamePics/a.jpg'); -fx-background-size: cover,auto;");
+    }
+
+    private void setEffectsCardImages(ImageView view) {
+        Bloom glow = new Bloom();
+        view.setOnMouseEntered(event -> {
+            glow.setThreshold(0.75);
+            view.setEffect(glow);
+        });
+        view.setOnMouseExited(event -> {
+            view.setEffect(null);
+        });
+        view.setOnMouseClicked(e->{
+        });
     }
 }
