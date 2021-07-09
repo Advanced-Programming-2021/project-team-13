13e2aaf894195a36da8ef78890c871716bf31603
MrNimaa <nima.na1991@gmail.com>
2021-07-09 16:21:33 +0430

    add coloradjust to summon

--- src/main/java/model/Cell.java
+++ src/main/java/model/Cell.java
@@ -6,6 +6,7 @@ import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.RotateTransition;
 import javafx.animation.Timeline;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
@@ -19,10 +20,11 @@ public class Cell {
     private StackPane picture;
     private Image monsterImage;
     ImageView cardImages;
-    private Timeline summon = new Timeline(new KeyFrame(Duration.seconds(0.1), e -> {
-        cardImages.setOpacity(cardImages.getOpacity() + 0.1);
+    private ColorAdjust colorAdjust = new ColorAdjust();
+    private ColorAdjust flipSummon = new ColorAdjust();
+    private Timeline summon = new Timeline(new KeyFrame(Duration.seconds(0.05), e -> {
+        colorAdjust.setBrightness(colorAdjust.getBrightness() - 0.05);
     }));
-
 
     Cell(Card card) {
         this.card = card;
@@ -55,17 +57,19 @@ public class Cell {
         if (attackOrDefense == AttackOrDefense.ATTACK
                 && getPicture().getRotate() == 90)
             getPicture().setRotate(0);
+        colorAdjust.setBrightness(1);
+        cardImages.setEffect(colorAdjust);
         cardImages.setFitWidth(93.3333);
         cardImages.setFitHeight(126.6666);
-        cardImages.setOpacity(0);
         this.picture.getChildren().add(cardImages);
-        summon.setCycleCount(10);
+        summon.setCycleCount(20);
         summon.play();
     }
 
     public void setPictureUP() {
         picture.getChildren().clear();
         ImageView cardImages = new ImageView(monsterImage);
+
         picture.getChildren().add(cardImages);
         cardImages.setFitWidth(93.3333);
         cardImages.setFitHeight(126.6666);
